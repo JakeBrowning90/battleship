2f3fd8d5e72da727eec7edf90a7943f477ee5653
jakebrowning90 <jacobbrowning90@gmail.com>
2023-02-17 12:34:33 -0500

    Implement function to check that ships are placed within board boundaries

--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -1,8 +1,10 @@
+import { Ship } from "./shipFactory";
 
 class Gameboard {
     constructor() {
         this.allSpaces = this.generateSpaces();
         // this.occupiedSpaces = placeShips();
+        this.occupiedSpaces = [];
         this.missedShots = [];
     }
 
@@ -11,9 +13,11 @@ class Gameboard {
         for (let i = 0; i < 10; i++) {
             let row = [];
             for (let j = 0; j < 10; j++) {
-                let cell = [];
-                cell.push(i);
-                cell.push(j);
+                let cell = {};
+                cell.xCoord = i;
+                cell.yCoord = j;
+                // Give each cell a "contains" property to indicate which ship is there?
+                cell.contains = null;
                 row[j] = cell;
             }
             playfield[i] = row;
@@ -22,7 +26,39 @@ class Gameboard {
     }
 
     placeShips() {
+        // Make 5 ships
+        let fleet = [new Ship("carrier", 5), new Ship("battleship", 4), new Ship("destroyer", 3), new Ship("submarine", 3), new Ship("patrol boat", 2)];
+        
+        // For each ship:
+        for (let i = 0; i < fleet.length; i++) {
+            // Get coordinates and horizontal/vertical orientation
+            let orientation = "h"
+            let placement = [0, 0];
+        
+            // Check that placement is on board
+            if (this.isShipOnBoard(placement, fleet[i].length, orientation) == true) {
 
+            }
+            // Check that placement does not conflict with other ships
+
+            // Push legal coordinates to occupiedSpaces
+
+        }
+    }
+
+    isShipOnBoard(startingCell, shipLength, orientation) {
+        if (orientation == 'h') {
+            if (startingCell[0] + shipLength <= 10) {
+                return true;
+            } else {
+                return false;  
+            }    
+        } else if (orientation == 'v') {
+            if (startingCell[1] + shipLength <= 10) {
+                return true;  
+            }
+        } 
+        
     }
 
     receiveAttack() {
--- src/gameboardFactory.test.js
+++ src/gameboardFactory.test.js
@@ -1,6 +1,13 @@
 import { Gameboard } from "./gameboardFactory";
 
-test('creates new 10x10 gameboard', () => {
-    expect(new Gameboard()).toEqual({allSpaces: [], missedShots: []});
+// test('creates new 10x10 gameboard', () => {
+//     expect(new Gameboard()).toEqual({allSpaces: [], missedShots: []});
+// });
+
+test('', () => {
+    expect(new Gameboard().isShipOnBoard([0, 0], 5, "h")).toBe(true);
 });
 
+test('', () => {
+    expect(new Gameboard().isShipOnBoard([6, 0], 5, "h")).toBe(false);
+});
